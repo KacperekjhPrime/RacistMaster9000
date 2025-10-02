fbe50e834556080acde955eb250b4f62f7888ab7
ChibiBibiBa <marcinstanisz43@interia.pl>
2025-10-02 20:37:09 +0200

    Addedd simpliest and ugliest schools display - no logic

---
+++ src/routes/Dashboard/Schools/+page.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+  import SchoolsTable from "$lib/components/tables/SchoolsTable.svelte";
+  import type { School } from "$lib/ts/models/models";
+
+  let schools: Array<School> = [
+    {
+      name: "Zespół Szkół Technicznych i Ogólnokształcących Imienia Jana Pawła II",
+      city: "Limanowa",
+      acronym: "ZSTiO",
+    },{
+      name: "Zespół Szkół Technicznych i Ogólnokształcących Imienia Jana Pawła II",
+      city: "Limanowa",
+      acronym: "ZSTiO",
+    },{
+      name: "Zespół Szkół Technicznych i Ogólnokształcących Imienia Jana Pawła II",
+      city: "Limanowa",
+      acronym: "ZSTiO",
+    },{
+      name: "Zespół Szkół Technicznych i Ogólnokształcących Imienia Jana Pawła II",
+      city: "Limanowa",
+      acronym: "ZSTiO",
+    },{
+      name: "Zespół Szkół Technicznych i Ogólnokształcących Imienia Jana Pawła II",
+      city: "Limanowa",
+      acronym: "ZSTiO",
+    },
+  ];
+</script>
+
+<SchoolsTable {schools}></SchoolsTable>
