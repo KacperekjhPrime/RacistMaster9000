852aa9aab1283c147ac2333e5c709dfa94a2703c
ChibiBibiBa <marcinstanisz43@interia.pl>
2025-10-02 18:37:13 +0200

    Created Dummy Thick Data - very important don't delete

---
+++ src/lib/dummyThickData.ts
@@ -0,0 +1,14 @@
+const DummyThickData = {
+  title: "We racing ur mom",
+  routes: [
+    { name: "Home", href: "/Dashboard" },
+    { name: "Gokarty", href: "/Dashboard/Gokarts" },
+    { name: "Info", href: "/Dashboard/Info" },
+    { name: "Queue", href: "/Dashboard/Queue" },
+    { name: "Schools", href: "/Dashboard/Schools" },
+    { name: "Tournaments", href: "/Dashboard/Tournaments" },
+  ],
+  copyright: "© 2025 Copyright: Misex, sex i gosex",
+};
+
+export default DummyThickData;
