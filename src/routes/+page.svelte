b1321b43e7ac3e91395b3a4329543d10bbfdfd3d
ChibiBibiBa <marcin_stanisz@interia.pl>
2025-10-02 13:09:08 +0200

    Started separating fron end routes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1>Welcome to RacistMaster9000</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">this</a> for us to race your mom</p>
