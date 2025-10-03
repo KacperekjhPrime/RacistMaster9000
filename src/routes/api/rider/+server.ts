a582e2e2b197463813095f9d36a4733c5f7674d8
Kacper Puto <kputo77@gmail.com>
2025-10-03 10:56:13 +0200

    Make statement names more meaningful

--- src/routes/api/rider/+server.ts
+++ src/routes/api/rider/+server.ts
@@ -3,23 +3,23 @@ import { insert, select } from "$lib/ts/queryBuilder";
 import { validateRequestJSON } from "$lib/ts/validation.server";
 import { error, json } from "@sveltejs/kit";
 
-const selectAllStatement = select('Riders', ['RiderId', 'Name', 'Surname'] as const)
+const selectAllRiders = select('Riders', ['RiderId', 'Name', 'Surname'] as const)
     .join('Schools', ['SchoolId', 'Name AS SchoolName', 'Acronym AS SchoolNameAcronym', 'City'] as const, 'SchoolId')
     .prepare();
 
-const selectOneStatement = select('Riders', ['RiderId', 'Name', 'Surname'] as const)
+const selectOneRider = select('Riders', ['RiderId', 'Name', 'Surname'] as const)
     .join('Schools', ['SchoolId', 'Name AS SchoolName', 'Acronym AS SchoolNameAcronym', 'City'] as const, 'SchoolId')
     .where('Riders.RiderId = ?')
     .prepare<[number]>();
 
-const insertStatement = insert('Riders', ['Name', 'Surname', 'SchoolId'] as const).prepare();
+const insertRider = insert('Riders', ['Name', 'Surname', 'SchoolId'] as const).prepare();
 
-const selectSchoolStatement = select('Schools', ['SchoolId'])
+const selectSchool = select('Schools', ['SchoolId'])
     .where('Schools.SchoolId = ?')
     .prepare<[number]>()
 
 export function GET({ url }): Response {
-    return json(selectAllOrOne(url.searchParams, selectAllStatement, selectOneStatement));
+    return json(selectAllOrOne(url.searchParams, selectAllRiders, selectOneRider));
 }
 
 export async function POST({ request }): Promise<Response> {
@@ -29,8 +29,8 @@ export async function POST({ request }): Promise<Response> {
         schoolId: 'number'
     });
 
-    if (selectSchoolStatement.get(schoolId) === undefined) error(404, 'School does not exist.');
+    if (selectSchool.get(schoolId) === undefined) error(404, 'School does not exist.');
 
-    const { lastInsertRowid } = insertStatement.run(name, surname, schoolId);
+    const { lastInsertRowid } = insertRider.run(name, surname, schoolId);
     return json({ id: lastInsertRowid });
 }
--- src/routes/api/school/+server.ts
+++ src/routes/api/school/+server.ts
@@ -3,21 +3,21 @@ import { insert, select } from "$lib/ts/queryBuilder";
 import { validateRequestJSON } from "$lib/ts/validation.server.js";
 import { json } from "@sveltejs/kit";
 
-const selectAllStatement = select('Schools', ['SchoolId', 'Name', 'Acronym', 'City'] as const)
+const selectAllSchools = select('Schools', ['SchoolId', 'Name', 'Acronym', 'City'] as const)
     .prepare();
 
-const selectOneStatement = select('Schools', ['SchoolId', 'Name', 'Acronym', 'City'] as const)
+const selectOneSchool = select('Schools', ['SchoolId', 'Name', 'Acronym', 'City'] as const)
     .where('Schools.SchoolId = ?')
     .prepare<[number]>();
 
-const insertStatement = insert('Schools', ['Name', 'Acronym', 'City'] as const).prepare();
+const insertSchool = insert('Schools', ['Name', 'Acronym', 'City'] as const).prepare();
 
 export function GET({ url }): Response {
-    return json(selectAllOrOne(url.searchParams, selectAllStatement, selectOneStatement));
+    return json(selectAllOrOne(url.searchParams, selectAllSchools, selectOneSchool));
 }
 
 export async function POST({ request }): Promise<Response> {
     const { name, acronym, city } = await validateRequestJSON(request, { name: 'string', acronym: 'string', city: 'string' });
-    const { lastInsertRowid } = insertStatement.run(name, acronym, city);
+    const { lastInsertRowid } = insertSchool.run(name, acronym, city);
     return json({ id: lastInsertRowid });
 }
