3896e751783bb1b032c9266d0f72501ffadf862f
ChibiBibiBa <marcinstanisz43@interia.pl>
2025-10-03 17:13:37 +0200

    made rough sketch of home page

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import favicon from "$lib/assets/favicon.svg";
+  import DummyThickData from "$lib/ts/dummyThickData/dummyThickData";
+  import "$lib/styles/global.css";
+
+  let { children } = $props();
+</script>
+
+<svelte:head>
+  <link rel="icon" href={favicon} />
+  <title>{DummyThickData.title}</title>
+</svelte:head>
+
+<main>
+  {@render children?.()}
+</main>
+
+<style>
+  main {
+    margin-left: 5.5rem;
+    width: calc(100% - 11rem);
+  }
+</style>
