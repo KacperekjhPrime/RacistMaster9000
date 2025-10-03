c4c634f245f6bbc6287223d77e416544cdb6a933
ChibiBibiBa <marcinstanisz43@interia.pl>
2025-10-03 17:29:06 +0200

    started making table to display tournaments

---
+++ src/lib/components/organisms/tables/TournamentTable.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import type { Tournament } from "$lib/ts/models/models";
+
+  let { tournaments }: { tournaments: Array<Tournament> } = $props();
+</script>
+
+<div class="table"></div>
+
+<style>
+  .table {
+    width: 100%;
+    height: fit-content;
+  }
+  .row {
+    width: calc(100% - 1rem);
+    height: 4rem;
+    padding: 0.5rem;
+  }
+</style>
