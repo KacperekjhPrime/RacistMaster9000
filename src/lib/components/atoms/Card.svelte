1643ebb0efaa0512edb21c658a800b442a9ecaef
ChibiBibiBa <marcinstanisz43@interia.pl>
2025-10-03 23:36:30 +0200

    Notification card near completion end for the day, still no logic

--- src/lib/components/atoms/Card.svelte
+++ src/lib/components/atoms/Card.svelte
@@ -4,28 +4,27 @@
 
 <div class="card">
   <div class="cardHeader">
-    <button><Icon size="100%" icon="close"></Icon></button>
+    <button><Icon size="100%" icon="close" fill="default"></Icon></button>
   </div>
   <div class="cardBody">
-    <div class="cardIcon"><Icon size="50%" icon="star"></Icon></div>
-    <div class="cardContents">
-      <h3 class="cardTitle">Title</h3>
-      <div class="cardText">
-        Your file is uploading right now. Just give us a second to finish your
-        upload.
-      </div>
-      <div class="cardActions">
-        <div>s</div>
-        <div>b</div>
-      </div>
+    <h3 class="cardTitle">Title</h3>
+    <div class="cardIcon"><Icon size="70%" icon="star" fill="info"></Icon></div>
+    <div class="cardText">
+      Your file is uploading right now. Just give us a second to finish your
+      upload.
+    </div>
+    <div class="cardActions">
+      <div>s</div>
+      <div>b</div>
     </div>
   </div>
 </div>
 
 <style>
   .card {
-    aspect-ratio: 2.5/1;
-    height: 13rem;
+    aspect-ratio: 3/1;
+    width: 30rem;
+    height: fit-content;
     border-radius: var(--borderRadius);
     box-shadow: black 0px 0px 0.5rem 0.05rem;
     background: radial-gradient(
@@ -40,7 +39,6 @@
     flex-direction: column;
     align-items: center;
     color: var(--background);
-    fill: var(--primary);
     padding: 0.5rem;
   }
   .card:hover {
@@ -57,37 +55,45 @@
     aspect-ratio: 1/1;
   }
   .cardBody {
+    padding-top: 1rem;
+    padding-bottom: 1rem;
     width: 100%;
-    display: flex;
+    display: grid;
+    grid-template-columns: 20% 80%;
   }
   .cardIcon {
-    width: 30%;
-    aspect-ratio: 1/1;
+    width: 100%;
+    max-height: 10rem;
     display: flex;
     justify-content: center;
     align-items: center;
-  }
-  .cardContents {
-    width: 70%;
-    display: flex;
-    flex-direction: column;
+    grid-column: 1;
+    grid-row: 2;
   }
   .cardTitle {
-    font-size: 1.4rem;
+    font-size: 1.3rem;
     font-weight: 700;
     letter-spacing: var(--letterSpacing);
     color: #fff;
     padding: 0;
+    margin: 0;
+    grid-column: 2;
+    grid-row: 1;
   }
   .cardText {
     color: #d9d9d9;
     width: 100%;
+    grid-column: 2;
+    grid-row: 2;
+    display: flex;
+    align-items: center;
   }
   .cardActions {
     width: 100%;
     display: flex;
     align-items: center;
     justify-content: space-evenly;
-    margin-top: 10%;
+    grid-column: 2;
+    grid-row: 3;
   }
 </style>
---
+++ src/lib/components/theming/colors.ts
@@ -0,0 +1,22 @@
+const Colors = {
+  inherit: {
+    class: "inherit",
+  },
+  info: {
+    class: "info",
+  },
+  warning: {
+    class: "warning",
+  },
+  error: {
+    class: "error",
+  },
+  success: {
+    class: "success",
+  },
+  default: {
+    class: "default",
+  },
+};
+
+export default Colors;
