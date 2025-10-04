2e3d0a94b40127ade7d7b18f10e8c5e1b8cf564c
marcin <murczak0@interia.pl>
2025-10-04 13:44:57 +0200

    Added tournament selector in dashboard

--- src/routes/Dashboard/+page.svelte
+++ src/routes/Dashboard/+page.svelte
@@ -3,7 +3,7 @@
     import { formatTime, type ControllerData } from "$lib/helper";
     import { onMount } from "svelte";
 
-    const address = resolve("/api/controllerEvents");
+    const controllerApi = resolve("/api/controllerEvents");
     let eventSource: EventSource | null = null;
     
     let { data } = $props();
@@ -12,11 +12,13 @@
     let controllerData: ControllerData | null = $state(data);
     let runTime: number = $state(0);
     let runState: RunState = $state("Not started");
-    let timer: number = 0;
+    let timer: any = 0;
     let errorStatus: string = $state("");
     let totalLaps: number = $derived(controllerData.numberOfLaps);
     let lapsLeft: number = $state(0);
     let lockStatus: boolean = false;
+    let tournaments: Array<any> = $state([]);
+    let selectedTournamentId: number | null = $state(null);
 
     function restartRun() {
         runTime = 0;
@@ -56,22 +58,34 @@
     }
 
     onMount(async () => {
-        eventSource = new EventSource(address);
+        eventSource = new EventSource(controllerApi);
         eventSource.addEventListener("update", event => {
             update(JSON.parse(atob(event.data)) as ControllerData);
         });
         eventSource.addEventListener("connectionError", event => {
             errorStatus = atob(event.data);
         });
+
+        const request = await fetch(resolve("/api/tournaments"));
+        const response = await request.json() as Array<any>;
+        tournaments = response.filter(tournament => tournament.TournamentStateId == 2);
     });
 </script>
 
 {#if errorStatus != ""}
     <h1>Błąd połączenia z kontrolerem</h1>
 {:else}
-<h3>{JSON.stringify(controllerData)}</h3>
-<h1>Strona główna</h1>
-<h2>Status: {runState}</h2>
-<h3>Timer: {formatTime(runTime)}</h3>
-<h3>Okrążenia: {totalLaps - lapsLeft}/{totalLaps}</h3>
+    <h3>{JSON.stringify(controllerData)}</h3>
+    <h1>Strona główna</h1>
+    <p>Wybierz wyścig:</p>
+    <select bind:value={selectedTournamentId}>
+        {#each tournaments as tournament}
+        <option value={tournament.TournamentId}>{tournament.Name}</option>
+        {/each}
+    </select>
+    <p>Kolejka:</p>
+    <br><br>
+    <h2>Status: {runState}</h2>
+    <h3>Timer: {formatTime(runTime)}</h3>
+    <h3>Okrążenia: {totalLaps - lapsLeft}/{totalLaps}</h3>
 {/if}
