5fccf235eeea4407d51970aa21d2dfebb122b1dd
Kacper Puto <kputo77@gmail.com>
2025-10-05 13:41:47 +0200

    Add gokarts API

---
+++ src/routes/api/gokarts/+server.ts
@@ -0,0 +1,34 @@
+import { insert, select } from '$lib/database/queryBuilder.server.js';
+import type { Assert } from '$lib/ts/helper.js';
+import { validateRequestJSON } from '$lib/ts/validation.server';
+import { json } from '@sveltejs/kit';
+
+const selectGokarts = select('Gokarts', ['GokartId AS gokartId', 'Name AS name'] as const)
+    .prepare();
+
+const insertGokart = insert('Gokarts', ['Name'] as const)
+    .prepare();
+
+export type GETResponse = {
+    gokartId: number,
+    name: string
+}[];
+
+export function GET() {
+    const data = selectGokarts.all();
+    type _ = Assert<GETResponse, typeof data>;
+    return json(data);
+}
+
+export type POSTResponse = {
+    id: number
+}
+
+export async function POST({ request }) {
+    const { name } = await validateRequestJSON(request, { name: 'string' });
+    const data = {
+        id: Number(insertGokart.run(name).lastInsertRowid)
+    };
+    type _ = Assert<POSTResponse, typeof data>;
+    return json(data);
+}
