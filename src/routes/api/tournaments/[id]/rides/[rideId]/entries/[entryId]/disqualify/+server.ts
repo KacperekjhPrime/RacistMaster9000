ac0ed896de604850be3c56af66ea9d77f5f97406
Kacper Puto <kputo77@gmail.com>
2025-10-05 13:34:51 +0200

    Add disqualifying

---
+++ src/routes/api/tournaments/[id]/rides/[rideId]/entries/[entryId]/disqualify/+server.ts
@@ -0,0 +1,19 @@
+import { getAndValidateEntryIDs, tryFinishRide } from "../../entriesShared.server";
+import { select, update } from "$lib/database/queryBuilder.server";
+import { db } from "$lib/database/database.server";
+import { RideEntryState } from "$lib/database/databaseSchema.server";
+
+const updateEntryState = update('RideEntries', ['RideEntryStateId'])
+    .where('RideEntries.RideEntryId = ?')
+    .prepare<[entryId: number]>();
+
+export async function POST({ params }) {
+    const { rideId, rideEntryId } = getAndValidateEntryIDs(params);
+
+    db.transaction(() => {
+        updateEntryState.run(RideEntryState.Disqualified, rideEntryId);
+        tryFinishRide(rideId);
+    })();
+
+    return new Response();
+}
